974ef15013373eec3e75eb6157d06a55b331a7a6
aimers <uxdevsupport@aimersinfosoft.com>
2015-07-20 00:13:26 +0530

    Update 11:59pm.sql

--- SQL/19July/11:59pm.sql
+++ SQL/19July/11:59pm.sql
@@ -6,3 +6,16 @@ INSERT INTO `bookingdb`.`echmt` (`ECMID`, `MPNID`, `VALUE`, `ACTIV`, `CRTDT`, `C
 INSERT INTO `bookingdb`.`echmt` (`ECMID`, `MPNID`, `VALUE`, `ACTIV`, `CRTDT`, `CRTBY`, `CHNDT`, `CHNBY`) VALUES (NULL, '28', '1', '1', '2015-07-19 00:00:00', '1', NULL, NULL);
 INSERT INTO `bookingdb`.`echmt` (`ECMID`, `MPNID`, `VALUE`, `ACTIV`, `CRTDT`, `CRTBY`, `CHNDT`, `CHNBY`) VALUES (NULL, '40', '1', '1', '2015-07-19 00:00:00', '1', NULL, NULL);
 INSERT INTO `bookingdb`.`echmt` (`ECMID`, `MPNID`, `VALUE`, `ACTIV`, `CRTDT`, `CRTBY`, `CHNDT`, `CHNBY`) VALUES (NULL, '43', '1', '1', '2015-07-19 00:00:00', '1', NULL, NULL);
+
+
+
+
+
+UPDATE `bookingdb`.`echmt` SET `VALUE` = 'Neurologist.png' WHERE `echmt`.`ECMID` = 11;
+UPDATE `bookingdb`.`echmt` SET `VALUE` = 'Homeopath.png' WHERE `echmt`.`ECMID` = 11;
+UPDATE `bookingdb`.`echmt` SET `VALUE` = 'dentist.png' WHERE `echmt`.`ECMID` = 10;
+UPDATE `bookingdb`.`echmt` SET `VALUE` = 'Surgeon.png' WHERE `echmt`.`ECMID` = 13;
+UPDATE `bookingdb`.`echmt` SET `VALUE` = 'cardiologist.png' WHERE `echmt`.`ECMID` = 14;
+UPDATE `bookingdb`.`echmt` SET `VALUE` = 'Eyespecialist.png' WHERE `echmt`.`ECMID` = 15;
+UPDATE `bookingdb`.`echmt` SET `VALUE` = 'Sexologist.png' WHERE `echmt`.`ECMID` = 16;
+UPDATE `bookingdb`.`echmt` SET `VALUE` = 'aurveda.png' WHERE `echmt`.`ECMID` = 9;
