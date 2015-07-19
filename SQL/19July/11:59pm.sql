0ad7ed1dd8c761a48bf7f2ad0cc1f0f2bf1cc402
aimers <uxdevsupport@aimersinfosoft.com>
2015-07-20 00:00:02 +0530

    Create 11:59pm.sql

---
+++ SQL/19July/11:59pm.sql
@@ -0,0 +1,8 @@
+INSERT INTO `bookingdb`.`echmt` (`ECMID`, `MPNID`, `VALUE`, `ACTIV`, `CRTDT`, `CRTBY`, `CHNDT`, `CHNBY`) VALUES (NULL, '27', '1', '1', '2015-07-19 00:00:00', '1', NULL, NULL);
+INSERT INTO `bookingdb`.`echmt` (`ECMID`, `MPNID`, `VALUE`, `ACTIV`, `CRTDT`, `CRTBY`, `CHNDT`, `CHNBY`) VALUES (NULL, '29', '1', '1', '2015-07-19 00:00:00', '1', NULL, NULL);
+INSERT INTO `bookingdb`.`echmt` (`ECMID`, `MPNID`, `VALUE`, `ACTIV`, `CRTDT`, `CRTBY`, `CHNDT`, `CHNBY`) VALUES (NULL, '36', '1', '1', '2015-07-19 00:00:00', '1', NULL, NULL);
+INSERT INTO `bookingdb`.`echmt` (`ECMID`, `MPNID`, `VALUE`, `ACTIV`, `CRTDT`, `CRTBY`, `CHNDT`, `CHNBY`) VALUES (NULL, '38', '1', '1', '2015-07-19 00:00:00', '1', NULL, NULL);
+INSERT INTO `bookingdb`.`echmt` (`ECMID`, `MPNID`, `VALUE`, `ACTIV`, `CRTDT`, `CRTBY`, `CHNDT`, `CHNBY`) VALUES (NULL, '47', '1', '1', '2015-07-19 00:00:00', '1', NULL, NULL);
+INSERT INTO `bookingdb`.`echmt` (`ECMID`, `MPNID`, `VALUE`, `ACTIV`, `CRTDT`, `CRTBY`, `CHNDT`, `CHNBY`) VALUES (NULL, '28', '1', '1', '2015-07-19 00:00:00', '1', NULL, NULL);
+INSERT INTO `bookingdb`.`echmt` (`ECMID`, `MPNID`, `VALUE`, `ACTIV`, `CRTDT`, `CRTBY`, `CHNDT`, `CHNBY`) VALUES (NULL, '40', '1', '1', '2015-07-19 00:00:00', '1', NULL, NULL);
+INSERT INTO `bookingdb`.`echmt` (`ECMID`, `MPNID`, `VALUE`, `ACTIV`, `CRTDT`, `CRTBY`, `CHNDT`, `CHNBY`) VALUES (NULL, '43', '1', '1', '2015-07-19 00:00:00', '1', NULL, NULL);
